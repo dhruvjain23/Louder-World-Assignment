6cbff0b4b8e62b31c69a5e4d090c000750c39d8b
Dhruv jain <dhruvjain01d@gmail.com>
2025-05-10 17:29:04 +0530

    completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,7 @@ dotenv.config();
 const app=express();
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173","https://louder-world-assignment-v3cr.vercel.app"],
     credentials:true
 }))
 
