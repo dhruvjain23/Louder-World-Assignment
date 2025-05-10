df3238733d73c59ac67fc54975c72e4512a5f942
Dhruv jain <dhruvjain01d@gmail.com>
2025-05-10 17:16:21 +0530

    update for deployment

--- fronted/src/axios.js
+++ fronted/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL:"http://localhost:3000/api",
+    baseURL:"https://louder-world-assignment.vercel.app/api" || "http://localhost:3000/api",
     withCredentials:true
 })
 
